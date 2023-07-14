025a58978f1120d8408270418b02b865da7c46fc
David Guerra <david.guerra@wizeline.com>
2023-07-14 12:32:30 -0600

    env variables

--- src/utils/ticketing_jira.py
+++ src/utils/ticketing_jira.py
@@ -1,8 +1,10 @@
+import os
+
 from jira import JIRA
 
 jira_connection = JIRA(
-    basic_auth=('starsteygsv@gmail.com', 'ATATT3xFfGF0c92k-MmmhiKfW9Ua_--Q3tLygF5LM4e1gN2qFQ0oCNZssVWneUQtT9G8qPA0U6U6_pnHxaSoBAqSrc-o0fGIXMasfXgZ00HP1IoH2S_XN2cGQ_dX2vlnaoEFvV1XDebhv4WUGJp4pKwPb4L4V6m8xKqHAAtQxdcdObYqmGngC5M=CEB50659'),
-    server="https://hackai.atlassian.net/"
+    basic_auth=(os.getenv("JIRA_EMAIL"), os.getenv("API_KEY")),
+    server=os.getenv("JIRA_SERVER")
 )
 
 
