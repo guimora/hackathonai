1211dd5988f3ec5329ce02508ebeb56f211924bf
Griss Esquivel Silva <grissell.esquivel@Grisss-MacBook-Pro.local>
2023-07-14 12:12:29 -0600

    Updated requirements file

--- src/tests/unit_tests.py
+++ src/tests/unit_tests.py
@@ -1,8 +1,5 @@
 import unittest
 from unittest.mock import patch
-from src.behaviour_generator import get_response_chatgpt
-from src.gui.reqPanel import RequirementsPanel
-from src.gui.resultPanel import ResultsPanel
 from src.gui.mainFrame import MainFrame
 
 
@@ -13,23 +10,16 @@ class TestMainFrame(unittest.TestCase):
 
     @patch('src.behaviour_generator.get_response_chatgpt')
     def test_on_ok(self, mock_get_response):
+        self.frame.panel_welcome.Hide()
+        self.frame.panel_requirements.Show()
         mock_get_response.return_value = 'test'
-
         self.frame.panel_requirements.txt_reqs.SetValue('test')
-
         self.frame.on_ok(None)
-
         self.assertEqual(self.frame.panel_requirements._visible, False)
         self.assertEqual(self.frame.panel_results._visible, True)
         self.assertEqual(self.frame.panel_results._txtResultsValue, 'test')
 
-    def testOnReturn(self):  # Test on return method of main frame class 
-
-        self._visible = False  # Set visibility to false 
-
-        self._txtResultsValue = ''  # Set text value to empty string 
-
-        self._onReturn()  # Call on return method 
-
-        # Verify if the tests passed or not: 
+    def testOnReturn(self):  # Test on return method of main frame class
+        self._visible = False  # Set visibility to false
+        self._txtResultsValue = ''  # Set text value to empty string
         self.assertEqual(self._visible, True)   # Visibility should be true after calling on return method
